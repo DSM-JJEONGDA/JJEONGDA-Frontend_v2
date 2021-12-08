a4b4031805ed27c4c8dce953bffe45aa52ed5969
강석현 <tjrgus0713@gmail.com>
2021-12-08 23:12:57 +0900

    [추가] 달력 페이지 추가

---
+++ src/pages/calendarPage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Calendar from '../../components/calendar';
+import * as S from './styles'
+function CalendarPage() {
+    return (
+        <S.Wrapper>
+            <Calendar />
+        </S.Wrapper>
+    )
+}
+export default CalendarPage;
---
+++ src/pages/calendarPage/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+    width: 100%;
+    background-color: #EFE8DE;
+    padding-bottom: 185px;
+`
