58bfd4a3b8743cc90817623ca47be023fe9262c0
강석현 <tjrgus0713@gmail.com>
2021-12-09 09:09:36 +0900

    [추가] 해당 달 1일의 요일을 맞추기 위해 빈 칸 추가

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import * as S from './styles'
 import ArrowLeft from '../../assets/imgaes/calendar/arrow_left.svg'
 import ArrowRigth from '../../assets/imgaes/calendar/arrow_right.svg'
+import { useNavigate } from 'react-router'
 
 function Calendar() {
     const Day: string[] = ['SUN', 'MON', 'TUE', 'WED', 'THU', "FRI", "SAT"]
@@ -11,13 +12,19 @@ function Calendar() {
     const [year, setYear] = useState(date.getFullYear())
     const dotw = date.getDay()
     const today = `${date.getFullYear()}-${date.getMonth() + 1}-${day}`
-    const [firstDOTW, setFirstDOTW] = useState(new Date(year, month - 1, 1).getDay())// 해당 달의 첫번째 요일
+    const firstDOTW = new Date(year, month - 1, 1).getDay()// 해당 달의 첫번째 요일
     const lastDay = new Date(year, month, 0).getDate();
     useEffect(() => {
         if (month > 12) {
             setMonth(1)
+            setYear(year + 1)
+        }
+        if (month < 1) {
+            setMonth(12)
+            setYear(year - 1)
         }
     }, [month])
+    const navigate = useNavigate();
     return (
         <S.Wrapper>
             <S.Month>{month}</S.Month>
@@ -29,8 +36,11 @@ function Calendar() {
             <S.CalendarContent>
                 <S.Arrow src={ArrowLeft} onClick={() => setMonth(month - 1)} />
                 <div>
+                    {Array(firstDOTW).fill(void 0).map((item, index) => (
+                        <S.EachBox style={{ border: 'none' }} />
+                    ))}
                     {Array(lastDay).fill(void 0).map((item, index) => (
-                        <S.EachBox style={today === `${year}-${month}-${index + 1}` ? { color: '#ffffff', backgroundColor: '#707070' } : {}} > {index + 1}</S.EachBox>
+                        <S.EachBox style={today === `${year}-${month}-${index + 1}` ? { color: '#ffffff', backgroundColor: '#707070' } : {}} onClick={() => navigate(`/diary?day=${year}-${month}-${index + 1}`)} > {index + 1}</S.EachBox>
                     ))}
                 </div>
                 <S.Arrow src={ArrowRigth} onClick={() => setMonth(month + 1)} />
