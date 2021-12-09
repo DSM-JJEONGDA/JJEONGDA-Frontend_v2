e27f5c08775eda3ca0819481cf0bd27f80d3aa78
강석현 <tjrgus0713@gmail.com>
2021-12-09 11:58:47 +0900

    [수정] 스타일 수정

--- src/pages/calendarPage/styles.ts
+++ src/pages/calendarPage/styles.ts
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const Wrapper = styled.section`
     width: 100%;
     background-color: #EFE8DE;
-    padding-bottom: 185px;
+    min-height: 100vh;
+    /* padding-bottom: 185px; */
 `
