31c78438150b1878fecf355a19da1c6dd236a7d7
강석현 <tjrgus0713@gmail.com>
2021-12-12 23:21:35 +0900

    [추가] 리스트 불러오기 추가

--- src/pages/calendarPage/index.tsx
+++ src/pages/calendarPage/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import Calendar from '../../components/calendar';
+import { BASE_URL } from '../../constant/api';
 import * as S from './styles'
 function CalendarPage() {
+    useEffect(() => {
+        axios.get(BASE_URL + '/list/1').then((res) => {
+            console.log(res)
+        })
+    }, [])
     return (
         <S.Wrapper>
             <Calendar />
