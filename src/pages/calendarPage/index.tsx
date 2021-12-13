f1f701d9b985f6186e1beb8b783d52ebf21ac72d
강석현 <tjrgus0713@gmail.com>
2021-12-13 11:04:21 +0900

    [수정] 일기 리스트를 자신의 userId로 불러오도록 추가

--- src/pages/calendarPage/index.tsx
+++ src/pages/calendarPage/index.tsx
@@ -1,16 +1,19 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Calendar from '../../components/calendar';
 import { BASE_URL } from '../../constant/api';
+import { reducerType } from '../../modules/redux/reducer';
 import * as S from './styles'
 function CalendarPage() {
+    const userId = useSelector((state: reducerType) => state.setuser.info.id)
     useEffect(() => {
-        axios.get(BASE_URL + '/list/1', {
+        axios.get(BASE_URL + '/list/' + userId, {
             headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` }
         }).then((res) => {
             console.log(res)
         })
-    }, [])
+    }, [userId])
     return (
         <S.Wrapper>
             <Calendar />
