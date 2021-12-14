ebbcd7b2006d77d6b4556701656c37cbdddcfc24
강석현 <tjrgus0713@gmail.com>
2021-12-14 09:06:33 +0900

    ?

--- src/pages/calendarPage/index.tsx
+++ src/pages/calendarPage/index.tsx
@@ -1,19 +1,7 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';
 import Calendar from '../../components/calendar';
-import { BASE_URL } from '../../constant/api';
-import { reducerType } from '../../modules/redux/reducer';
 import * as S from './styles'
 function CalendarPage() {
-    const userId = useSelector((state: reducerType) => state.setuser.info.id)
-    useEffect(() => {
-        axios.get(BASE_URL + '/list/' + userId, {
-            headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` }
-        }).then((res) => {
-            console.log(res)
-        })
-    }, [userId])
     return (
         <S.Wrapper>
             <Calendar />
